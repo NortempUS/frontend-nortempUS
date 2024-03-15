d9879faa1dbcf42cb2f548a0d11cbf8b153cd275
JMGarCas <jmgarciacasillas135@gmail.com>
2024-03-15 14:29:37 +0100

    feat: Initial configuration of the project

---
+++ src/app/layout.jsx
@@ -0,0 +1,17 @@
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
+
+export const metadata = {
+  title: "Task4Task",
+  description: "Hoy por ti, mañana por mi",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Task4Task</h1>
+    </main>
+  );
+}
