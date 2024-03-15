f09ed51e4dfb66208901eca4f6d09e8db5b8559a
JMGarCas <jmgarciacasillas135@gmail.com>
2024-03-15 19:27:24 +0100

    feat: Add create service button

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,22 +1,32 @@
-import Card from "../ui/card/card"
-
+import Card from "../ui/card/card";
+import Link from "next/link";
 export default function Page() {
   return (
-    <div className='flex flex-col gap-2'>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+    <div className="flex flex-col gap-2">
+      <div className="flex justify-end items-center">
+        <Link
+          href="/"
+          className="bg-blue-500 hover:bg-blue-600 text-white py-1 px-2 rounded-md content lg:w-[300px] text-center"
+        >
+          Crear Servicio
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
-  )
+  );
 }
