60e0783d90de0711ee66f4a1d21c60d86191d8cf
JMGarCas <jmgarciacasillas135@gmail.com>
2024-03-16 01:12:09 +0100

    feat: fetch data

--- src/app/ui/chat/message.jsx
+++ src/app/ui/chat/message.jsx
@@ -1,17 +1,23 @@
 import clsx from "clsx";
 
-export default function Message({ sender }) {
+export default function Message({ message }) {
+  const sender = message.user === 1;
   return (
     <div className="w-full">
       <div className={clsx("flex", sender ? "justify-end" : "justify-start")}>
         <div
           className={clsx(
-            "w-5/12 m-2 my-3 h-20 p-3 rounded-md",
+            "flex flex-col justify-between w-5/12 m-2 my-3 h-20 p-3 rounded-md",
             sender ? "bg-blue-500" : "bg-gray-200"
           )}
         >
           <p className={clsx(sender ? "text-white" : "text-black")}>
-            Hola, ¿cómo estás?
+            {message.message}
+          </p>
+          <p className="flex justify-end">
+            <small className={clsx(sender ? "text-white" : "text-black")}>
+              {message.timestamp}
+            </small>
           </p>
         </div>
       </div>
