ce87e734fe608119334cb301a450fee0813a235b
JMGarCas <jmgarciacasillas135@gmail.com>
2024-03-18 19:42:43 +0100

    feat: PWA

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,11 +6,19 @@ const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 export const metadata = {
   title: "Task4Task",
   description: "Hoy por ti, mañana por mi",
+  manifest: "/manifest.json",
+  icons: {
+    apple: "/icon.png",
+  },
+  themeColor: "#ffffff",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className={`${poppins.className} bg-gray-200`}>{children}</body>
     </html>
   );
